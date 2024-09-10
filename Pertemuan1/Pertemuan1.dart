8c5d771dd297e7a6beeeaafdef90306b35cf291e
Hasna <151807926+anakabif4@users.noreply.github.com>
2024-09-10 19:48:40 +0700

    Update Pertemuan1.dart

--- Pertemuan1/Pertemuan1.dart
+++ Pertemuan1/Pertemuan1.dart
@@ -1,1 +1,13 @@
+void main() {
+    int angka = 17;
+    print('Angka $angka adalah ${cekJenis(angka)}.');
+}
+
+String cekJenis(int angka) {
+    if (angka % 2 == 0) {
+        return 'Genap';
+    } else {
+        return 'Ganjil';
+    }
+}
 
